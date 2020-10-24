e4e2fbd12638aad5a5473392aadb7e367c4f139d
adilbaev-ruslan <adilbaev-ruslan@umail.uz>
2020-10-24 23:05:44 +0500

    jkdf

--- app/src/main/java/com/example/redbook/data/RedBookDatabase.kt
+++ app/src/main/java/com/example/redbook/data/RedBookDatabase.kt
@@ -11,7 +11,7 @@ import com.example.redbook.data.model.RedBook
 abstract class RedBookDatabase: RoomDatabase() {
 
     private lateinit var INSTANCE: RedBookDatabase
-    //databaceBook
+    //databaceBookdfsf
 
     fun getInstance(context: Context) : RedBookDatabase =
         Room.databaseBuilder(
